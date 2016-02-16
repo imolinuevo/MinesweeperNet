1003d488f393023dab1504a0334edafa80280227
Iñigo Molinuevo <i.m.parron@opendeusto.es>
2016-02-16 19:23:09 +0100

    Play again button update and cell coloring.

--- Minesweeper/MainForm.cs
+++ Minesweeper/MainForm.cs
@@ -12,9 +12,41 @@ namespace Minesweeper
 {
     public partial class MainForm : Form
     {
+        private Button[,] cellGrid;
+
         public MainForm()
         {
             InitializeComponent();
+            cellGrid = new Button[9, 9];
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    cellGrid[i, j] = new Button();
+                    cellGrid[i, j].Text = "";
+                    cellGrid[i, j].FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+                    cellGrid[i, j].Margin = new System.Windows.Forms.Padding(0);
+                    cellGrid[i, j].Size = new System.Drawing.Size(30, 30);
+                    cellGrid[i, j].Click += new EventHandler(cellClick);
+                    cellGrid[i, j].Image = global::Minesweeper.Properties.Resources.tile;
+                    tableLayoutPanel.Controls.Add(cellGrid[i, j]);
+                }
+            }
+            this.ActiveControl = playAgain;
+        }
+
+        private void playAgainClick(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void cellClick(object sender, EventArgs e)
+        {
+            ((Button) sender).FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            ((Button)sender).Text = "1";
+            ((Button)sender).ForeColor = Color.Red;
+            //((Button)sender).Enabled = false;
+            ((Button)sender).Image = null;
         }
     }
 }
